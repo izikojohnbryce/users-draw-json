edc772a092d9c13a004096681e50437f12ffea0e
Itzik <izikosher@gmail.com>
2020-07-22 23:20:30 +0300

    app adding promise

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 let input = document.getElementById('select-user-count');
+
+let handleChange = (e) => new Promise((resolve, reject) =>{
+    window.localStorage.setItem("userCount" , e.target.value); // saving the user input
