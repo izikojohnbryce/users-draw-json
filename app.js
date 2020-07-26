f07116a7496ccfae03b47546d9a85118eeceb496
Itzik <izikosher@gmail.com>
2020-07-26 20:45:42 +0300

    design

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ let handleChange = (e) => new Promise((resolve, reject) =>{
         }).map((value)=>{
             return `
             <div class="card">
-                <img src="${value.pic}" width="250px" height="250px">
+                <img src="${value.pic}" class="card-pic">
                 <h1>${value.name}</h1>
                 <small>${value.location}</small>
                 <small>${value.phone}</small>
                 <button type="button">Language!</button>
-            
+                <div id="languages-grid" class="languages-grid" ></div>
             </div>
 
             `
